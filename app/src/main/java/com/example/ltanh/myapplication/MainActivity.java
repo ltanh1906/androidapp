9417c08714ff784b9b1eba727da1e1bbf3c3c61e
ltanh1906 <ltanh196@gmail.com>
2024-11-03 00:04:52 +0700

    Tạo ứng dụng gán hình nền ngẫu nhiên

--- app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
@@ -5,54 +5,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtNumber;
-    Button btnRandom;
-    EditText edtNumber1, edtNumber2;
+
+    LinearLayout linearLayout;
+    ArrayList<Integer> arrayImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = (LinearLayout) findViewById(R.id.myLinearlayout);
 
-        AnhXa();
+        arrayImage = new ArrayList<>();
+        arrayImage.add(R.drawable.img11);
+        arrayImage.add(R.drawable.img2);
+        arrayImage.add(R.drawable.img3);
 
+        Random random = new Random();
+        int index = random.nextInt(arrayImage.size());
 
-
-        btnRandom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String chuoi1 = edtNumber1.getText().toString().trim();
-                String chuoi2 = edtNumber2.getText().toString().trim();
-                if(chuoi1.isEmpty() || chuoi2.length() == 0){
-                    Toast.makeText(MainActivity.this, "Vui lòng nhập đủ số!",Toast.LENGTH_SHORT).show();
-                }else{
-                    //ép kiểu
-                    int min = Integer.parseInt(chuoi1);
-                    int max = Integer.parseInt(chuoi2);
-
-                    //tạo số ngẫu nghiên qua hàm random
-                    Random random = new Random();
-                    int number = random.nextInt(max-min +1)+min; //random trong khoảng 0 đến 99
-                    //ép số sang chuỗi
-//                txtNumber.setText(number + "");
-                    txtNumber.setText(String.valueOf(number));
-                }
+        linearLayout.setBackgroundResource(arrayImage.get(index));
 
 
-            }
-        });
+
     }
 
-    private void AnhXa(){
-        txtNumber = (TextView) findViewById(R.id.textViewNumber);
-        btnRandom = (Button) findViewById(R.id.buttonRandom);
-        edtNumber1 = (EditText) findViewById(R.id.editTextNumber1);
-        edtNumber2 = (EditText) findViewById(R.id.editTextNumber2);
-    }
 }
