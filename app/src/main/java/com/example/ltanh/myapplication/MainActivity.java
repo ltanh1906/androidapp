c11cffbd682ae2b569ca33ab4e512ddbf9262ad0
ltanh1906 <ltanh196@gmail.com>
2024-11-09 18:14:36 +0700

    CountDownTimer với ProgressBar

--- app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.ltanh.myapplication;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,27 +16,55 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
-    ArrayList<Integer> arrayImage;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private CountDownTimer countDownTimer;
+    private int totalTime = 10000; // Thời gian đếm ngược (10 giây)
+    private int interval = 100; // Cập nhật thanh tiến trình mỗi 100 ms
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        linearLayout = (LinearLayout) findViewById(R.id.myLinearlayout);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.textView);
 
-        arrayImage = new ArrayList<>();
-        arrayImage.add(R.drawable.img11);
-        arrayImage.add(R.drawable.img2);
-        arrayImage.add(R.drawable.img3);
-
-        Random random = new Random();
-        int index = random.nextInt(arrayImage.size());
-
-        linearLayout.setBackgroundResource(arrayImage.get(index));
-
-
-
+        startCountDown();
     }
+
+    private void startCountDown() {
+        progressBar.setProgress(100); // Đặt giá trị ban đầu cho ProgressBar là 100%
+
+        countDownTimer = new CountDownTimer(totalTime, interval) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Tính toán phần trăm còn lại
+                int progress = (int) (millisUntilFinished * 100 / totalTime);
+                progressBar.setProgress(progress);
+
+                // Hiển thị thời gian còn lại (tính theo giây)
+                textView.setText(millisUntilFinished / 1000 + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                // Khi hết giờ, thiết lập ProgressBar về 0 và TextView thành "Hết giờ"
+                progressBar.setProgress(0);
+                textView.setText("Hết giờ");
+            }
+        };
+
+        countDownTimer.start(); // Bắt đầu bộ đếm ngược
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
 
 }
