190e685549c1cc14b69fe8b9946bd824d5807a90
ltanh1906 <ltanh196@gmail.com>
2024-11-16 23:16:08 +0700

    Context Menu

--- app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ltanh/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.ltanh.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private ArrayList<String> studentInfoList;
     private Classroom classroom;
-
+    private TextView contextMenuTarget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +37,41 @@ public class MainActivity extends AppCompatActivity {
                 showPopupMenu(v);
             }
         });
+        // Tìm TextView để hiển thị Context Menu
+        contextMenuTarget = (TextView) findViewById(R.id.contextMenuTarget);
+
+        // Đăng ký Context Menu cho TextView
+        registerForContextMenu(contextMenuTarget);
+    }
+    // Tạo Context Menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        // Kiểm tra View đang hiển thị Context Menu
+        if (v.getId() == R.id.contextMenuTarget) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+        }
     }
 
+    // Xử lý sự kiện khi chọn một mục trong Context Menu
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_option1:
+                Toast.makeText(this, "Bạn chọn Tùy chọn 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.context_option2:
+                Toast.makeText(this, "Bạn chọn Tùy chọn 2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.context_option3:
+                Toast.makeText(this, "Bạn chọn Tùy chọn 3", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
     private void showPopupMenu(View view) {
         // Tạo PopupMenu
         PopupMenu popupMenu = new PopupMenu(this, view);
